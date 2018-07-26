7bd1fae100d9844ecde0d9bc5e40f1976b0a29d1
ScrublordBG <megata10@gmail.com>
2018-07-26 15:13:21 +0300

    Initial setup

---
+++ src/main/java/com/techmark/techmarkwebsite/TechmarkwebsiteApplication.java
@@ -0,0 +1,12 @@
+package com.techmark.techmarkwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechmarkwebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechmarkwebsiteApplication.class, args);
+    }
+}
